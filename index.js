09c50d664b8e26915287259143b446db58ddd7b7
Y Sai Koushik Reddy <152732336+KoushikReddy9963@users.noreply.github.com>
2025-06-13 23:52:23 +0530

    clean code without unused vars

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.get('/api/test2', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
